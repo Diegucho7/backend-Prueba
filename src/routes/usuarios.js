92272491c45973efe93b4e5c049b92c34fd44b88
Diegucho7 <87243460+Diegucho7@users.noreply.github.com>
2025-01-31 03:01:56 -0500

    validar campos

--- src/routes/usuarios.js
+++ src/routes/usuarios.js
@@ -1,10 +1,20 @@
 const { Router } = require("express");
-const { getUser, postUser, putUser, deleteUser } = require("../controller/usuariosController")
+const { getUser, postUser, putUser, deleteUser } = require("../controller/usuariosController");
+const { check } = require('express-validator');
+const { validarCampos } = require("../middleware/validar-campos");
+
 
 const router = Router();
 
 router.get('/', getUser);
-router.post('/', postUser);
+router.post('/', [
+    check('name', 'El nombre es obligatorio').not().isEmpty(),
+    check('last_name', 'El apellido es obligatorio').not().isEmpty(),
+    check('phone', 'El telefono es obligatorio').not().isEmpty(),
+    check('email', 'El email es obligatorio').isEmail(),
+    check('address', 'La direccion es obligatoria').not().isEmpty(),
+    validarCampos
+], postUser);
 router.put('/:id', putUser);
 router.delete('/:id', deleteUser);
 
